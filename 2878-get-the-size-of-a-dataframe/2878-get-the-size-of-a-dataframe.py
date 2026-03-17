f2f1b4ee106ab62c59d48cebb1cba566fa5565b9
Pascual Felicio <pascualfelicio@gmail.com>
2026-03-17 12:50:18 +0100

    Time: 293 ms (23.83%), Space: 66.7 MB (12.12%) - LeetHub

--- 2878-get-the-size-of-a-dataframe/2878-get-the-size-of-a-dataframe.py
+++ 2878-get-the-size-of-a-dataframe/2878-get-the-size-of-a-dataframe.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def getDataframeSize(players: pd.DataFrame) -> List[int]:
-    
+    df = pd.DataFrame(players)
+    rows = len(df)
+    columns = len(df.columns)
+    df_size = [rows, columns]
+    return df_size
