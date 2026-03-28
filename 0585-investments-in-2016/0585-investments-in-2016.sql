302f38c7b0d8ec557c51390a77b663a9cf649046
Pascual Felicio <pascualfelicio@gmail.com>
2026-03-28 15:26:58 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,16 +1,9 @@
-# Write your MySQL query statement below
-
 SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM Insurance
-WHERE tiv_2015 IN (
-    SELECT tiv_2015
+FROM (
+    SELECT 
+        *,
+        COUNT(*) OVER(PARTITION BY tiv_2015) AS tiv_count,
+        COUNT(*) OVER(PARTITION BY lat, lon) AS loc_count
     FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(*) > 1
-)
-AND (lat, lon) IN (
-    SELECT lat, lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*) = 1
-)
+) AS stats
+WHERE tiv_count > 1 AND loc_count = 1
